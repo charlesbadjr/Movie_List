bf0d180b957ad5b5d238061a38cbf8ade90bc697
Charlie <charlesbadjr@gmail.com>
2019-07-31 00:58:05 -0600

    rails is the worst

--- app/controllers/shop_lists_controller.rb
+++ app/controllers/shop_lists_controller.rb
@@ -8,8 +8,26 @@ class ShopListsController < ApplicationController
   
   end
 
+  def create
+    @shoplist = Shoplists.new(shoplist.params)
+    
+    if @shoplist.save
+      redirect_to shoplist_path(@shoplist)
+    else
+      render :new
+    end
+  end
+
+
   def new
   
   end
+
+  private
+
+  def shop_params
+    params.require(:shoplist).permit(:item, :gift, :link)
+  end
+
 
 end
